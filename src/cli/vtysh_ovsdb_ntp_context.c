7d6c4a9e81f80005433c52b0c9fea392f14bde6a
Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>
2016-04-15 10:43:17 -0700

    chg: dev: Static analysis fixes for NTP
    Change-Id: I57e3aca4d93beccc1ebda5813e19b85ae91c96cd
    Signed-off-by: Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>

--- src/cli/vtysh_ovsdb_ntp_context.c
+++ src/cli/vtysh_ovsdb_ntp_context.c
@@ -63,8 +63,7 @@ vtysh_config_context_ntp_clientcallback(void *p_private)
 
     /* Generate CLI for the NTP_Association Table */
     OVSREC_NTP_ASSOCIATION_FOR_EACH(ntp_assoc_row, p_msg->idl) {
-        strcpy(str_temp, "");
-
+        memset(str_temp, 0, sizeof(str_temp));
         if (NULL != ntp_assoc_row->key_id) {
             snprintf(str_temp, sizeof(str_temp), " key-id %ld", ((struct ovsrec_ntp_key *)ntp_assoc_row->key_id)->key_id);
         }
