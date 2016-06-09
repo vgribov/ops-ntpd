73fe527bc0696a585a3203ee4c49c89ad02668b7
Raksha Madhava Bangera <raksha.madhava-bangera@hpe.com>
2016-06-08 17:19:25 -0700

    Custom validator for NTP
    Tags:add, dev
    
    Added custom validator to validate ip address in NTP_Association table.
    
    Change-Id: I685a58d2fd220aaac911ad7d3f31ad603fd6fe01
    Signed-off-by: Raksha Madhava Bangera <raksha.madhava-bangera@hpe.com>

---
+++ opsplugins/ntp_association.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from opsvalidator.base import BaseValidator
+from opsvalidator import error
+from opsvalidator.error import ValidationError
+from opsrest.utils.utils import get_column_data_from_row
+import ipaddress
+
+
+class NtpAssociationValidator(BaseValidator):
+    resource = "ntp_association"
+
+    def validate_modification(self, validation_args):
+        ntp_association_row = validation_args.resource_row
+        if hasattr(ntp_association_row, "address"):
+            ip_address = get_column_data_from_row(ntp_association_row,
+                                                  "address")
+            if (not ipaddress.is_valid_ip_address(ip_address)):
+                details = "Invalid IP address %s." % (ip_address)
+                raise ValidationError(error.VERIFICATION_FAILED, details)
